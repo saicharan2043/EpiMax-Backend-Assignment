58d6543f43834b5737f1b8814cb2dfe04869a60a
saicharan2043 <saikann2043@gmail.com>
2024-05-01 17:56:42 +0530

    changed admin get api getAllData to getAllUsersData

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.get("/getData", verifyUserIdentity, async (req, resp) => {
 });
 
 // for admin
-app.get("/getAllData", verifyUserIdentity, async (req, resp) => {
+app.get("/getAllUsersData", verifyUserIdentity, async (req, resp) => {
   const createQuery = `select tasks.id as taskId , username , title,  description , status , assignee_id , created_at , updated_at from users inner join tasks on users.id = tasks.assignee_id order by updated_at desc`;
   const response = await db.all(createQuery);
   resp.send(response);
